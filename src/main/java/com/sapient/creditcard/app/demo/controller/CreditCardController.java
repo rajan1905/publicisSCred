d3fd4fc18986b75bc8b8423b05b45c032769adcd
rajan_1905@live.com <rajan_1905@live.com>
2020-03-09 22:14:40 +0000

    Working on back-end

--- src/main/java/com/sapient/creditcard/app/demo/controller/CreditCardController.java
+++ src/main/java/com/sapient/creditcard/app/demo/controller/CreditCardController.java
@@ -2,11 +2,14 @@ package com.sapient.creditcard.app.demo.controller;
 
 import com.sapient.creditcard.app.demo.dto.CreditCard;
 import com.sapient.creditcard.app.demo.repository.CCRepository;
-import com.sapient.creditcard.app.demo.utility.CCUtility;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CreditCardController {
@@ -14,13 +17,33 @@ public class CreditCardController {
     @Autowired
     private CCRepository ccRepository;
 
-    @PostMapping
+    @PostMapping(path = "addCard")
     public ResponseEntity addCreditCard(@RequestParam String name,
                                         @RequestParam String cardNo,
                                         @RequestParam int limit){
         ResponseEntity response = new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
 
-        
+        Optional<CreditCard> card = ccRepository.findCreditCardByCardNo(cardNo);
+
+        if(!card.isPresent()){
+            CreditCard creditCard = CreditCard.builder()
+                    .cardNo(cardNo)
+                    .balance(limit)
+                    .name(name)
+                    .build();
+
+            CreditCard cardSave = ccRepository.saveAndFlush(creditCard);
+
+            if(cardSave != null){
+                response = new ResponseEntity(HttpStatus.OK);
+            }
+        }
         return response;
+    }
+
+    @GetMapping(path = "getAllCards")
+    @Cacheable(cacheNames = "allCardsCache")
+    public List<CreditCard> getAllCards(){
+        return ccRepository.findAll();
     }
 }
--- src/main/java/com/sapient/creditcard/app/demo/repository/CCRepository.java
+++ src/main/java/com/sapient/creditcard/app/demo/repository/CCRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface CCRepository extends JpaRepository<CreditCard, String> {
+    Optional<CreditCard> findCreditCardByCardNo(String cardNo);
 }
